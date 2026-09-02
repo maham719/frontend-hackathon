493e5a039ec5b28ce865bd872d1457ca61d245f5
Syeda Maham Amjad <163040767+maham719@users.noreply.github.com>
2026-09-03 03:26:43 +0500

    Update socket connection URL

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://backend-hackathon-seven.vercel.app", {
+const socket = io("https://backend-hackathon-1dgv.onrender.com", {
     withCredentials: true,
     autoConnect: false
 });
